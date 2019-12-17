ee6c34ce3504add4df7323651fede3d972b9d8e7
FredaZzzz <freda_zh@hotmail.com>
2019-12-17 17:00:41 -0500

    Update test2.java

--- test2.java
+++ test2.java
@@ -22,13 +22,16 @@ import org.neo4j.io.fs.FileUtils;
 public class test2 {
 
   private enum RelTypes implements RelationshipType{
-	  CALLS, ExternalCall
+	  CALLS, ExternalCall, CONTAINS
   }
   
-  public static Node findNode(ArrayList<Node> list, String entry_point) {
-	  for (Node function:list) {
-		  if(function.getProperty("entry_point").equals(entry_point)) {
-			  return function;
+  public static Node findNode(ArrayList<Node> list, String property, String value) {
+	  //check which node contains property with value
+	  //assume that only one item in the list matches the value
+	  //return the (first) one
+	  for (Node item:list) {
+		  if(item.getProperty(property).equals(value)) {
+			  return item;
 		  }
 	  }
 	  return null;
@@ -41,8 +44,15 @@ public class test2 {
 	GraphDatabaseService db= dbFactory.newEmbeddedDatabase(database);
 	ArrayList<Node> functions = new ArrayList<Node>();
 	ArrayList<Relationship> calls = new ArrayList<Relationship>();
+	ArrayList<Node> files = new ArrayList<Node>();
+	ArrayList<Relationship> contains = new ArrayList<Relationship>();
+	int fileID = 0;
+	Node tempFile;
 	//File file = new File("C:\\workspace\\test2Neo4j\\src\\test2Neo4j\\input2.csv"); 
-	File file = new File("C:\\workspace\\test2Neo4j\\src\\test2Neo4j\\adventurev5_stripped.csv"); 
+	//File file = new File("C:\\workspace\\test2Neo4j\\src\\test2Neo4j\\adventurev5_stripped.csv"); 
+	//File file = new File("C:\\workspace\\test2Neo4j\\src\\test2Neo4j\\libtinfo.so.5.csv"); 
+	File file = new File("C:\\workspace\\test2Neo4j\\src\\test2Neo4j\\1.exe.csv"); 
+	String fileName = "libtinfo.so.5";
 	Scanner sc = new Scanner(file);
 	String header = sc.next();
 	sc.useDelimiter("\\n"); 
@@ -50,49 +60,80 @@ public class test2 {
 		db.execute("MATCH (n)\r\nDETACH DELETE n\r\n");
 		while(sc.hasNext()) {
 			String line = sc.next().replace("\n", "").replace("\r", "");
-			String[] columns = line.split(";",5);
-			Node newFunc = findNode(functions, columns[3]);
+			String[] columns = line.split(";",8);
+			Node newFunc = findNode(functions, "name", columns[0]);
 			if (newFunc==null) {
 				newFunc = db.createNode();
-				newFunc.setProperty("entry_point", columns[3]);
+				newFunc.setProperty("name", columns[0]);
 				functions.add(newFunc);
 			}
-			newFunc.setProperty("name", columns[0]);
-			//System.out.println(columns[1]);
 			int complexity = Integer.parseInt(columns[1]);
 			newFunc.setProperty("complexity", complexity);
-			newFunc.setProperty("pagerank", (complexity+10)*2);			
-			String parameterString = columns[2].substring(1, columns[2].length() - 1);
+			newFunc.setProperty("pagerank", (complexity+10)*2);	
+			newFunc.setProperty("prototype", columns[2]);
+			newFunc.setProperty("size", columns[3]);
+			if(newFunc.hasProperty("entry_point")) {
+				Object prevEntryPoint = newFunc.getProperty("entry_point");
+				newFunc.setProperty("entry_point", prevEntryPoint+", "+columns[7]);
+			}
+			else {
+				newFunc.setProperty("entry_point", columns[5]);
+				
+			}
+			newFunc.setProperty("entry_point", columns[5]);
+			newFunc.setProperty("fileName", columns[7]);
+			//distinguish lib func
+			if (columns[7].equals(fileName)) {
+				newFunc.setProperty("isLib", 0);
+			}
+			else {
+				newFunc.setProperty("isLib", 1);
+			}
+			//distinguish between files
+			int id = 0;
+			int num = 0;
+			tempFile = findNode(files, "name", columns[7]);
+			if(tempFile != null) {
+				id = (int) tempFile.getProperty("id");
+				num = (int)tempFile.getProperty("funcNum");
+				tempFile.setProperty("funcNum", num+1);
+			}
+			else {
+				tempFile = db.createNode();
+				tempFile.setProperty("id", fileID);
+				fileID ++;
+				tempFile.setProperty("name", columns[7]);
+				tempFile.setProperty("funcNum", 1);
+				tempFile.addLabel(Label.label("File"));
+				files.add(tempFile);
+			}
+			Relationship fileContains = tempFile.createRelationshipTo(newFunc, RelTypes.CONTAINS);
+			contains.add(fileContains);
+			newFunc.setProperty("fileID", tempFile.getProperty("id"));
+			
+			String parameterString = columns[4].substring(1, columns[4].length() - 1);
 			if (parameterString.length() != 0) {
-				//String[] parameters = parameterString.split(", ",-1);
 				String parameters = parameterString.replace("[", "").replace("]", "");
 				newFunc.setProperty("parameters", parameters);
 			}
 			else {
 				newFunc.setProperty("parameters", "");
-			}
-			String calleeString = columns[4].substring(1, columns[4].length() - 1);
+			}			
+			String calleeString = columns[6].substring(1, columns[6].length() - 1);
 			if (calleeString.length() != 0) {
 				String[] callees = calleeString.split(", ",-1);
 				for(String callee: callees) {
-					Node calleeFunc = findNode(functions, callee);
+					Node calleeFunc = findNode(functions, "name", callee);
 					if (calleeFunc == null) {
 						calleeFunc = db.createNode();
-						calleeFunc.setProperty("entry_point", callee);
+						calleeFunc.setProperty("name", callee);
 						functions.add(calleeFunc);
 					}
 					Relationship newRel = newFunc.createRelationshipTo(calleeFunc, RelTypes.CALLS);
 					calls.add(newRel);	
 				}
 			}			
-			if(columns[0].contains("FUN_")) {
-				newFunc.addLabel(Label.label("Function"));
-				newFunc.setProperty("community", "Function");
-			}
-			else {
-				newFunc.addLabel(Label.label("LibFunction"));
-				newFunc.setProperty("community", "LibFunction");
-			}
+			newFunc.addLabel(Label.label("Function"));
 		}
 		tx.success();
 	}
